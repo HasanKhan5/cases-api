68a8af4ed371455ea27fe31d567d29f93206cad1
HasanKhan5 <hasankhanf7@gmail.com>
2026-03-16 03:25:59 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, HTTPException, Request
+import json
+
+app = FastAPI()
+
+API_KEY = "123456789"   # give this to Salesforce developer
+FILE_NAME = "data.json"
+
+
+@app.get("/cases")
+def get_cases(api_key: str):
+
+    if api_key != API_KEY:
+        raise HTTPException(status_code=403, detail="Invalid API Key")
+
+    with open(FILE_NAME, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    return data
